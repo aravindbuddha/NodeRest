4cf585a0db55138c97bf91ddbb844a0d392693d3
Aravind Buddha <buddhaaravind@gmail.com>
2013-11-26 20:27:02 +0530

    addes

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
-var 
+var mysql = require('mysql'),
 http = require("http"),
 url = require("url"),
-rq="",
-rs="",
+con="",rq="",rs="",
 app={
 	init:function(){
-		this.startServer();
+		this.startHTTP();
+		this.startMySql();
 	},
-	startServer:function(){
+	startHTTP:function(){
 		var base = this;
 		http.createServer(function(req, res) {
 			rq=req;rs=res;
+			console.log(rq.headers.host);
   		rs.writeHead(200, {"Content-Type": "application/json"});
   		base.route(url.parse(rq.url).pathname);
   		rs.end();
 		}).listen(8888);
 	},
+	startMySql:function(){
+		con = mysql.createConnection({
+  		host     : 'localhost',
+  		user     : 'root',
+  		password : '',
+		});
+		con.connect(function(err) {
+		  if(err){
+		  	console.log(err);
+		  }
+		  else{
+		  	console.log("Connected to mysql");
+		  	this.createDB();
+		  }
+		});
+	},
+	createDB:function(){
+		CREATE DATABASE IF NOT EXISTS NodeRest
+	},
 	route:function(path){
-		rs.write(path);
+		var base=this;
+		if(path == "/user"){
+			base.getUsers();
+		}	
+	},
+	getUsers:function(){
+
 	}
 };
 app.init();
